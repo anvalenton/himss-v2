adbb2205925d4ac272864ff09a20021257d4721a
an <anvalenton@gmail.com>
2021-10-05 15:33:45 -0400

    updated app js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cors({
 // Have Node serve the files for our built React app
 if (process.env.NODE.ENV === 'production') {
 
-    app.use(express.static(path.resolve(__dirname, './client/build')));
+    app.use(express.static('.client/build'));
 
     // All other GET requests not handled before will return our React app
     app.get('*', (req, res) => {
